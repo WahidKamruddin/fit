1c749dac267a3b41bd593cbec685345f19df0201
Wahid Kamruddin <wahidkamruddin101@gmail.com>
2025-03-27 01:39:07 -0400

    feat!(ui): added shadcn 2.3.0

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,94 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
+    darkMode: ['class'],
+    content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        'mocha' : {
-          100: '#E9DDC8',
-          150: '#BCAC92',
-          200: '#D9D0C1',
-          250: '#D2CBC3',
-          300: '#CDB38F',
-          400: '#A28769',
-          500: '#634832'
-        },
-        
-        'off-white' : {
-          100: '#F9F9F9'
-        },
-
-        'olive' : {
-          100: '#ACB09D',
-          200: '#626355'
-        }
-      },
-      fontFamily: {
-        nunito: ['Nunito']        
-      }
-    },
+  	extend: {
+  		colors: {
+  			mocha: {
+  				'100': '#E9DDC8',
+  				'150': '#BCAC92',
+  				'200': '#D9D0C1',
+  				'250': '#D2CBC3',
+  				'300': '#CDB38F',
+  				'400': '#A28769',
+  				'500': '#634832'
+  			},
+  			'off-white': {
+  				'100': '#F9F9F9'
+  			},
+  			olive: {
+  				'100': '#ACB09D',
+  				'200': '#626355'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		},
+  		fontFamily: {
+  			nunito: [
+  				'Nunito'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 export default config

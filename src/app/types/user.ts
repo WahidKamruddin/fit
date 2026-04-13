b5bc0b6b85569a4c8d95522371b8956774e4cfc1
Wahid Kamruddin <wahidkamruddin101@gmail.com>
2026-04-12 23:27:39 -0400

    refactor(shadcn): deleted unused shadcn UI files

--- src/app/types/user.ts
+++
@@ -1,9 +0,0 @@
-import type { Vibe, Style, Weather } from './clothing';
-
-export interface UserPreferences {
-  preferred_vibes: Vibe[];
-  preferred_styles: Style[];
-  avoid_vibes: Vibe[];
-  climate: Weather[];
-  comfort_priority: number;
-}
